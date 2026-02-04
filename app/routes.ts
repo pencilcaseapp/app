8204d9e2936b89b98b208fb1555d8685945a3a82
Henrik Fricke <henrikfricke@web.de>
2026-02-04 21:49:35 +0100

    refa: clean up routes file

--- app/routes.ts
+++ app/routes.ts
@@ -1,11 +1,11 @@
 import { type RouteConfig, index, layout, route } from '@react-router/dev/routes';
 
 export default [
-  index('routes/index.tsx'),
-  route('home', 'routes/home.tsx'),
-  route('new', 'routes/new.tsx'),
+  index('routes/index/index.ts'),
+  route('home', 'routes/home/index.ts'),
+  route('new', 'routes/new/index.ts'),
   layout('layouts/editor/editor.tsx', [
-    route('doc/:id', 'routes/doc.tsx'),
+    route('doc/:id', 'routes/doc/index.ts'),
   ]),
 
 ] satisfies RouteConfig;
