42e3397f57a30abea29e1b70d6042311a93df51f
Henrik Fricke <henrikfricke@web.de>
2026-03-02 18:57:01 +0100

    feat: fixed editor toolbar (at least for desktop 😅)

--- app/ui/editor/editor.test.tsx
+++ app/ui/editor/editor.test.tsx
@@ -13,17 +13,3 @@ test('renders placeholder', async () => {
 
   expect(await findByText('Start here …')).toBeInTheDocument();
 });
-
-test('renders avatars', async () => {
-  const { findByText } = render(
-    <Editor
-      placeholder={<div>Start here …</div>}
-      ariaPlaceholder="Start here …"
-      avatars={['Alice', 'Bob', 'Charlie']}
-    />,
-  );
-
-  expect(await findByText('Alice')).toBeInTheDocument();
-  expect(await findByText('Bob')).toBeInTheDocument();
-  expect(await findByText('Charlie')).toBeInTheDocument();
-});
