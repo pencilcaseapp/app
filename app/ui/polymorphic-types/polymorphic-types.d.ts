0326ee02a25736e49da32f89cf9596181f251e93
André <andre.rusakow@googlemail.com>
2026-03-02 22:22:37 +0100

    refactor(ui/polymorphic-types): make linter happy

--- app/ui/polymorphic-types/polymorphic-types.d.ts
+++ app/ui/polymorphic-types/polymorphic-types.d.ts
@@ -11,18 +11,24 @@ type AsProp<C extends ElementType> = {
 
 export type PropsToOmit<C extends ElementType, P> = keyof (AsProp<C> & P);
 
-// A polymorphic component prop that can be used to override the default element type.
+/*
+* A polymorphic component prop that can
+* be used to override the default element type.
+*/
 export type PolymorphicComponentProp<
   C extends ElementType,
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   Props = {},
-> = PropsWithChildren<Props & AsProp<C>> &
-  Omit<ComponentPropsWithoutRef<C>, PropsToOmit<C, Props>>;
+> = PropsWithChildren<Props & AsProp<C>>
+  & Omit<ComponentPropsWithoutRef<C>, PropsToOmit<C, Props>>;
 
 // Same as above, but with a "ref" prop
 export type PolymorphicComponentPropWithRef<
   C extends ElementType,
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   Props = {},
-> = PolymorphicComponentProp<C, Props> & { ref?: PolymorphicRef<C> };
+> = PolymorphicComponentProp<C, Props>
+  & { ref?: PolymorphicRef<C> };
 
-export type PolymorphicRef<C extends ElementType> =
-  ComponentPropsWithRef<C>['ref'];
+export type PolymorphicRef<C extends ElementType>
+  = ComponentPropsWithRef<C>['ref'];
