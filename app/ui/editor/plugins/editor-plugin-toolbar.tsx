a9bc1f5655c8102defff83579ba4aa19e99a602a
André <andre.rusakow@googlemail.com>
2026-03-25 22:52:11 +0100

    refactor(ui/topbar): remove shadow and handle refs

--- app/ui/editor/plugins/editor-plugin-toolbar.tsx
+++ app/ui/editor/plugins/editor-plugin-toolbar.tsx
@@ -2,12 +2,13 @@ import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext
 import { $createParagraphNode, $getSelection, $isRangeSelection, BLUR_COMMAND, COMMAND_PRIORITY_CRITICAL, FORMAT_TEXT_COMMAND, SELECTION_CHANGE_COMMAND } from 'lexical';
 import { $createHeadingNode } from '@lexical/rich-text';
 import { $setBlocksType } from '@lexical/selection';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { $getFormatBlock } from '../utils/node';
 import { Button, Toggle, Topbar } from '~/ui';
 import type { EditorFormatBlock } from '../editor.types';
-import { useWindowScroll } from 'react-use';
+import { useMedia, useWindowScroll } from 'react-use';
 import classNames from 'classnames';
+import { useVirtualKeyboard } from '~/hooks/use-virtual-keyboard';
 
 export const EditorPluginToolbar: React.FC = () => {
   const [editor] = useLexicalComposerContext();
@@ -17,6 +18,9 @@ export const EditorPluginToolbar: React.FC = () => {
     italic: false,
     underline: false,
   });
+
+  const topbarRef = useRef<HTMLElement>(null);
+  const [isVirtualKeyboardOpen] = useVirtualKeyboard();
 
   useEffect(() => {
     editor.registerCommand(
@@ -41,6 +45,9 @@ export const EditorPluginToolbar: React.FC = () => {
     editor.registerCommand(
       BLUR_COMMAND,
       () => {
+        if (!topbarRef.current) {
+          return false;
+        }
         setTextFormat({
           bold: false,
           italic: false,
@@ -85,38 +92,49 @@ export const EditorPluginToolbar: React.FC = () => {
     });
   }, [editor]);
 
+  useEffect(() => {
+    if (!topbarRef.current || !isVirtualKeyboardOpen) {
+      return;
+    }
+  }, [isVirtualKeyboardOpen]);
+
   const handleEditorFocus = useCallback((e: React.MouseEvent) => {
     e.preventDefault();
   }, []);
 
+  const isWide = useMedia('(min-width: 480px)');
   const { y } = useWindowScroll();
   const isScrolling = y > 65;
 
   return (
     <Topbar
-      left={<Button color="secondary" icon="sidebar" iconTitle="sidebar" />}
-      right={<Button icon="share">Share</Button>}
+      ref={topbarRef}
+      hasBorder={isVirtualKeyboardOpen}
+      left={!isVirtualKeyboardOpen ? <Button color="secondary" icon="sidebar" iconTitle="sidebar" /> : <Button color="secondary" icon="close" className="text-pca-grey-400!" iconTitle="close" />}
+      right={!isVirtualKeyboardOpen && (<Button icon="share" color="upgrade">Share</Button>)}
       center={(
-        <div className={classNames('flex gap-2 origin-center transition-all ring-pca-grey-200 bg-pca-white/85 dark:bg-pca-grey-900/80 rounded-2xl px-1.5 py-1.5 mx-1',
-          isScrolling && 'lg:shadow-xs lg:ring-1 lg:dark:ring-pca-grey-700 lg:backdrop-blur-md',
-        )}
-        >
-          <div className="flex gap-4 md:gap-2">
-            <Toggle className="h-8!" isActive={formatBlock === 'h1'} onMouseDown={handleEditorFocus} onClick={() => toggleBlock('h1')} icon="h1" iconTitle="headline 1" />
-            <Toggle className="h-8!" isActive={formatBlock === 'h2'} onMouseDown={handleEditorFocus} onClick={() => toggleBlock('h2')} icon="h2" iconTitle="headline 2" />
-            <Toggle className="h-8!" isActive={formatBlock === 'h3'} onMouseDown={handleEditorFocus} onClick={() => toggleBlock('h3')} icon="h3" iconTitle="headline 3" />
+        (isWide || isVirtualKeyboardOpen) && (
+          <div className={classNames('flex gap-2 origin-center transition-all ring-pca-grey-200 bg-pca-white/85 dark:bg-pca-grey-900/80 rounded-2xl px-1.5 py-1.5 mx-1',
+            isScrolling && 'lg:shadow-xs lg:ring-1 lg:dark:ring-pca-grey-700 lg:backdrop-blur-md',
+          )}
+          >
+            <div className="flex gap-4 md:gap-2">
+              <Toggle className="h-8!" isActive={formatBlock === 'h1'} onMouseDown={handleEditorFocus} onClick={() => toggleBlock('h1')} icon="h1" iconTitle="headline 1" />
+              <Toggle className="h-8!" isActive={formatBlock === 'h2'} onMouseDown={handleEditorFocus} onClick={() => toggleBlock('h2')} icon="h2" iconTitle="headline 2" />
+              <Toggle className="h-8!" isActive={formatBlock === 'h3'} onMouseDown={handleEditorFocus} onClick={() => toggleBlock('h3')} icon="h3" iconTitle="headline 3" />
+            </div>
+            <div className="flex gap-4 md:gap-2">
+              <Toggle className="h-8!" isActive={textStyle.bold} onMouseDown={handleEditorFocus} onClick={() => toggleTextStyle('bold')} icon="bold" iconTitle="bold" />
+              <Toggle className="h-8!" isActive={textStyle.italic} onMouseDown={handleEditorFocus} onClick={() => toggleTextStyle('italic')} icon="italic" iconTitle="italic" />
+              <Toggle className="h-8!" isActive={textStyle.underline} onMouseDown={handleEditorFocus} onClick={() => toggleTextStyle('underline')} icon="underline" iconTitle="underline" />
+            </div>
+            <div className="flex gap-4 md:gap-2">
+              <Toggle className="h-8!" disabled isActive={false} icon="listUl" iconTitle="unordered list" />
+              <Toggle className="h-8!" disabled isActive={false} icon="listOl" iconTitle="ordered list" />
+              <Toggle className="h-8!" disabled isActive={false} icon="listCheck" iconTitle="checklist" />
+            </div>
           </div>
-          <div className="flex gap-4 md:gap-2">
-            <Toggle className="h-8!" isActive={textStyle.bold} onMouseDown={handleEditorFocus} onClick={() => toggleTextStyle('bold')} icon="bold" iconTitle="bold" />
-            <Toggle className="h-8!" isActive={textStyle.italic} onMouseDown={handleEditorFocus} onClick={() => toggleTextStyle('italic')} icon="italic" iconTitle="italic" />
-            <Toggle className="h-8!" isActive={textStyle.underline} onMouseDown={handleEditorFocus} onClick={() => toggleTextStyle('underline')} icon="underline" iconTitle="underline" />
-          </div>
-          <div className="flex gap-4 md:gap-2">
-            <Toggle className="h-8!" disabled isActive={false} icon="listUl" iconTitle="unordered list" />
-            <Toggle className="h-8!" disabled isActive={false} icon="listOl" iconTitle="ordered list" />
-            <Toggle className="h-8!" disabled isActive={false} icon="listCheck" iconTitle="checklist" />
-          </div>
-        </div>
+        )
       )}
     />
   );
