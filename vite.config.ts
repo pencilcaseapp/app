9ee54637c97c81ca639b128de4f52f53d60c7708
Henrik Fricke <henrikfricke@web.de>
2026-04-13 19:14:57 +0200

    fix: bring back vite chrome dev tools json (#87)

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
+import devtoolsJson from 'vite-plugin-devtools-json';
 
 const isStorybook = process.argv[1]?.includes('storybook');
 
@@ -11,5 +12,11 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     !isStorybook && reactRouter(),
+    devtoolsJson(),
   ],
+  optimizeDeps: {
+    entries: [
+      'app/routes/*.tsx',
+    ],
+  },
 });
