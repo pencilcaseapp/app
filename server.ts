cc173b667b6385d41e82d39f2628ef55d8ceab54
Henrik Fricke <henrikfricke@web.de>
2026-05-04 22:32:04 +0200

    fix(live server): use crosswv after hocuspocus v4 update

--- server.ts
+++ server.ts
@@ -1,20 +1,21 @@
 import express from 'express';
-import expressWebsockets from 'express-ws';
 import compression from 'compression';
 import { createRequestHandler } from '@react-router/express';
 import { internalIpV4 } from 'internal-ip';
 import { createLiveServer } from '~/live';
 import { getConfig } from '~/config';
 import { migrateDatabase } from '~/db/migrate';
+import { createServer } from 'node:http';
 
-const { app } = expressWebsockets(express());
+const app = express();
+const server = createServer(app);
 const config = getConfig();
 
 app.use(compression());
 app.disable('x-powered-by');
 
 if (config.environment === 'prod') {
-  createLiveServer(app);
+  createLiveServer(server);
   app.use(
     '/assets',
     express.static('build/client/assets', { immutable: true, maxAge: '1y' }),
@@ -33,9 +34,9 @@ else {
     }),
   );
 
-  app.ws('/live', async (websocket, request) => {
-    const { hocuspocus } = await viteDevServer.ssrLoadModule('./app/live/index.ts');
-    hocuspocus.handleConnection(websocket, request);
+  server.on('upgrade', async (request, socket, head) => {
+    const { ws } = await viteDevServer.ssrLoadModule('./app/live/index.ts');
+    ws.handleUpgrade(request, socket, head);
   });
 
   app.use(viteDevServer.middlewares);
@@ -56,7 +57,7 @@ async function startServer() {
   const localIp = await internalIpV4();
   const host = localIp ?? config.server.host;
 
-  app.listen(config.server.port, config.server.host, () => {
+  server.listen(config.server.port, config.server.host, () => {
     console.log(`🚀 App listening on http://localhost:${config.server.port}`);
     console.log(`🛜 Address on your network http://${host}:${config.server.port}`);
   });
