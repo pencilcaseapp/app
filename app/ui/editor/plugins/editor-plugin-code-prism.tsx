29fcf01d466ac3441e0f774505c539ea87919d6f
Henrik Fricke <henrikfricke@web.de>
2026-05-11 15:50:34 +0200

    fix: prism is not defined

--- app/ui/editor/plugins/editor-plugin-code-prism.tsx
+++ app/ui/editor/plugins/editor-plugin-code-prism.tsx
@@ -1,3 +1,6 @@
+import 'prismjs';
+import 'prismjs/components/prism-javascript';
+
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
 import { registerCodeHighlighting } from '@lexical/code-prism';
 import { useEffect } from 'react';
