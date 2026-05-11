8b9441cff3a566492a474e0993f1f837714fc13a
Henrik Fricke <henrikfricke@web.de>
2026-05-11 17:41:22 +0200

    fix: apply editor layout body classes

--- app/layouts/editor.tsx
+++ app/layouts/editor.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router';
 import { SocketClientProvider } from '~/contexts/socket-client';
 
 export const handle = {
-  bodyClass: 'w-full bg-pca-white dark:bg-pca-grey-900',
+  bodyClassName: 'w-full bg-pca-white dark:bg-pca-grey-900',
 };
 
 export default function LayoutEditor() {
