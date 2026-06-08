fb24c40c2f04fdfd4dea12c9922f0b6a31d09da4
Henrik Fricke <henrikfricke@web.de>
2026-06-08 14:21:30 +0200

    refa(label): improve disabled handling

--- app/ui/label/label.tsx
+++ app/ui/label/label.tsx
@@ -1,4 +1,3 @@
-import classNames from 'classnames';
 import { Typography } from '../typography/typography';
 import type { PropsWithChildren } from 'react';
 
@@ -9,17 +8,14 @@ export interface Label extends PropsWithChildren {
 
 export const Label: React.FC<Label>
   = ({ htmlFor, disabled, children }) => {
-    const classes = classNames(disabled && '!text-pca-grey-300 dark:!text-pca-grey-800');
-
     return (
       <Typography
         htmlFor={htmlFor}
         as="label"
         variant="bodySmall"
         fontWeight="semibold"
-        textColorLight="grey-900"
-        textColorDark="grey-300"
-        className={classes}
+        textColorLight={disabled ? 'grey-300' : 'grey-900'}
+        textColorDark={disabled ? 'grey-800' : 'grey-300'}
       >
         {children}
       </Typography>
