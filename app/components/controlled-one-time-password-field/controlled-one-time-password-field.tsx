d7a6eb7109be72aaf666805522d5ac57abdd9e72
Henrik Fricke <henrikfricke@web.de>
2026-06-16 16:35:50 +0200

    fix: otp immediately submits form on iOS

--- app/components/controlled-one-time-password-field/controlled-one-time-password-field.tsx
+++ app/components/controlled-one-time-password-field/controlled-one-time-password-field.tsx
@@ -1,28 +1,20 @@
 import { useFieldContext } from '~/contexts/form';
 import { OneTimePasswordField, type OneTimePasswordFieldProps } from '~/ui/one-time-password-field/one-time-password-field';
 
-export type ControlledOneTimePasswordFieldProps = Omit<OneTimePasswordFieldProps, 'onValueChange' | 'value' | 'onAutoSubmit' | 'errorMessage'>;
+export type ControlledOneTimePasswordFieldProps = Omit<OneTimePasswordFieldProps, 'onValueChange' | 'value' | 'onAutoSubmit' | 'errorMessage' | 'name'>;
 
 // eslint-disable-next-line @stylistic/max-len
 export const ControlledOneTimePasswordField: React.FC<ControlledOneTimePasswordFieldProps>
   = (props) => {
     const field = useFieldContext<string>();
 
-    const handleAutoSubmit = () => {
-      dispatchEvent(new Event('submit', { cancelable: true, bubbles: true }));
-    };
-
     return (
-      <>
-        <OneTimePasswordField
-          value={field.state.value}
-          onValueChange={field.handleChange}
-          onAutoSubmit={handleAutoSubmit}
-          errorMessage={field.state.meta.errors[0]?.message}
-          onBlur={field.handleBlur}
-          {...props}
-        />
-        <input id={field.name} type="hidden" name={field.name} value={field.state.value} />
-      </>
+      <OneTimePasswordField
+        name={field.name}
+        value={field.state.value}
+        onValueChange={field.handleChange}
+        errorMessage={field.state.meta.errors[0]?.message}
+        {...props}
+      />
     );
   };
