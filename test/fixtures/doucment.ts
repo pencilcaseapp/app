9b67e0072359f29df767990a027c22c24b75efba
Henrik Fricke <henrikfricke@web.de>
2026-07-27 10:44:18 +0200

    feat: create new document with owner

---
+++ test/fixtures/doucment.ts
@@ -0,0 +1,13 @@
+import { faker } from '@faker-js/faker';
+import type { Document } from '~/repos/document';
+
+faker.seed(42);
+
+export const documentFixture: Document = {
+  id: faker.string.uuid(),
+  title: faker.lorem.words(3),
+  content: null,
+  createdAt: faker.date.past(),
+  updatedAt: faker.date.past(),
+  userId: null,
+};
