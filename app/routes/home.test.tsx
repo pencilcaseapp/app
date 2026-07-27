4f73a90c7bed932b4a326a3c96bcb74f59a20401
Henrik Fricke <henrikfricke@web.de>
2026-07-27 15:48:15 +0200

    feat(home route): redirect to last updated doc if uses is logged in

--- app/routes/home.test.tsx
+++ app/routes/home.test.tsx
@@ -1,4 +1,8 @@
+import { href, RouterContextProvider } from 'react-router';
 import { beforeEach, expect, test, vi } from 'vitest';
+import { optionalUserSessionContext } from '~/contexts/user-session';
+import { documentFixture } from '~/test/fixtures/doucment';
+import { userFixture } from '~/test/fixtures/user';
 import { renderRoute } from '~/utils/testing';
 
 const redirectMock = vi.fn();
@@ -13,12 +17,60 @@ vi.mock('react-router', async () => {
   };
 });
 
+const getDocumentListMock = vi.fn();
+vi.mock('~/repos/document', async () => ({
+  getDocumentList: (userId: string) => getDocumentListMock(userId),
+}));
+
 beforeEach(() => {
   vi.clearAllMocks();
 });
 
-test('redirects to new doc', async () => {
-  await renderRoute('/home');
+test('redirects to new doc if no user session exists', async () => {
+  const context = new RouterContextProvider();
+  context.set(optionalUserSessionContext, null);
+
+  await renderRoute('/home', {
+    params: {},
+    context,
+  });
 
   expect(redirectMock).toHaveBeenCalledWith('/new');
+});
+
+test('redirects to new doc if user session exists but no documents', async () => {
+  const context = new RouterContextProvider();
+  context.set(optionalUserSessionContext, userFixture);
+  getDocumentListMock.mockResolvedValue([]);
+
+  await renderRoute('/home', {
+    params: {},
+    context,
+  });
+
+  expect(redirectMock).toHaveBeenCalledWith('/new');
+});
+
+test('redirects to last updated doc if user session exists', async () => {
+  const context = new RouterContextProvider();
+  context.set(optionalUserSessionContext, userFixture);
+  getDocumentListMock.mockResolvedValue([
+    {
+      ...documentFixture,
+      id: 'doc-1',
+    },
+    {
+      ...documentFixture,
+      id: 'doc-2',
+    },
+  ]);
+
+  await renderRoute('/home', {
+    params: {},
+    context,
+  });
+
+  expect(redirectMock).toHaveBeenCalledWith(
+    href('/doc/:id', { id: 'doc-1' }),
+  );
 });
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,15 @@
 import { href, redirect } from 'react-router';
+import type { Route } from './+types/home';
+import { optionalUserSessionContext } from '~/contexts/user-session';
+import { getDocumentList } from '~/repos/document';
 
-export function loader() {
+export async function loader({ context }: Route.LoaderArgs) {
+  const user = context.get(optionalUserSessionContext);
+  const documentList = user ? await getDocumentList(user.id) : [];
+
+  if (documentList.length > 0) {
+    return redirect(href('/doc/:id', { id: documentList[0].id }));
+  }
+
   return redirect(href('/new'));
 }
