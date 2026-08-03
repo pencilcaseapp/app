4b523912fe5f50e2397aaee0a70cdb9036dc0b95
André <andre.rusakow@googlemail.com>
2026-08-03 15:59:13 +0200

    fix(ui/sidebar-context): provide closeOnNavigate for tablet and mobile

--- app/ui/sidebar-context/sidebar-context.ts
+++ app/ui/sidebar-context/sidebar-context.ts
@@ -2,8 +2,9 @@ import { createContext, type RefObject } from 'react';
 
 export type SidebarContextType = {
   isSidebarOpen: boolean;
-  setIsSidebarOpen: (isSidebarOpen: boolean) => void;
   triggerRef: RefObject<HTMLButtonElement | null>;
+  setIsSidebarOpen: (isSidebarOpen: boolean) => void;
+  closeOnNavigate: () => void;
 };
 
 export const SidebarContext = createContext<SidebarContextType | null>(null);
